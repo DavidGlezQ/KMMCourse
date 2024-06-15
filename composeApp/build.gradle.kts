b28da482facf00367055fcd893162b10baaee9b7
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2024-06-15 14:21:20 -0600

    Navigation with voyager
    Voyager Navigation and transitions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -41,6 +41,9 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            //Voyager Navigation
+            implementation(libs.voyager.navigator)
+            implementation(libs.voyager.transitions)
         }
     }
 }
--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,7 +1,13 @@
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -12,22 +18,59 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import cafe.adriel.voyager.transitions.FadeTransition
+import cafe.adriel.voyager.transitions.ScaleTransition
+import cafe.adriel.voyager.transitions.SlideTransition
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var name: String by remember { mutableStateOf("") }
-        Column(
-            modifier = Modifier.fillMaxSize().padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+        Navigator(screen = MainScreen()) { navigator ->
+            SlideTransition(navigator)
+            //FadeTransition(navigator)
+            //ScaleTransition(navigator)
+        }
+    }
+}
+
+class MainScreen : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
-            OutlinedTextField(value = name, onValueChange = { name = it })
-            AnimatedVisibility (name.isNotEmpty()) {
-                Text("Hello $name")
+            Button(modifier = Modifier, onClick = { navigator.push(SecondScreen()) }) {
+                Text("Navigate to Second Screen")
             }
         }
     }
+}
+
+class SecondScreen : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Column (
+            modifier = Modifier.fillMaxSize().background(Color.LightGray),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Second Screen", fontSize = 26.sp, color = Color.White)
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(modifier = Modifier, onClick = { navigator.pop() }) {
+                Text("Back")
+            }
+        }
+    }
+
 }
