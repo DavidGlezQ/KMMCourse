79bb2224a35043eb9a9b5f0a8e968a70b82865fe
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2024-06-21 23:22:03 -0600

    simple style

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,7 +1,9 @@
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -15,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
@@ -35,7 +38,10 @@ fun App() {
         var superHeroName by remember { mutableStateOf("") }
         var superheroList by remember { mutableStateOf<List<Hero>>(emptyList()) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Row {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
                 OutlinedTextField(value = superHeroName, onValueChange = { superHeroName = it })
                 Button(onClick = { getSuperHeroList(superHeroName) { superheroList = it } }) {
                     Text("Load")
