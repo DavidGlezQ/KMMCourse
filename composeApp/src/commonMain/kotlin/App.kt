6538056cd8ac5c58bd7a75f5bdd572bd021950f9
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2024-06-21 23:20:11 -0600

    Response as a list

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,6 +1,9 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -12,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import kotlinx.coroutines.CoroutineScope
@@ -19,23 +23,30 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.launch
 import network.NetworkUtils.httpClient
+import network.model.ApiResponse
+import network.model.Hero
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        val url = "https://www.superheroapi.com/api.php/cfc55c379d941f22661c7167750d91b9/search/a"
-        var response by remember { mutableStateOf("Waiting...") }
+        //var response by remember { mutableStateOf("Waiting...") }
         var superHeroName by remember { mutableStateOf("") }
+        var superheroList by remember { mutableStateOf<List<Hero>>(emptyList()) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Row {
                 OutlinedTextField(value = superHeroName, onValueChange = { superHeroName = it })
-                Button(onClick = { getSuperHeroList(superHeroName) }) {
+                Button(onClick = { getSuperHeroList(superHeroName) { superheroList = it } }) {
                     Text("Load")
                 }
             }
-            Button(onClick = {
+            LazyColumn {
+                items(superheroList) { hero ->
+                    Text(modifier = Modifier.fillMaxWidth(), text = hero.name)
+                }
+            }
+            /*Button(onClick = {
                 response = "Loading..."
                 CoroutineScope(Dispatchers.IO).launch {
                     val testResponse = httpClient.get(url)
@@ -43,17 +54,19 @@ fun App() {
                 }
             }) {
                 Text("Call SuperHero API!")
-            }
+            }*/
             //Text(response)
         }
     }
 }
 
-fun getSuperHeroList(superHeroName: String) {
+fun getSuperHeroList(superHeroName: String, onSuccessResponse: (List<Hero>) -> Unit) {
     if (superHeroName.isBlank()) return
+    val url =
+        "https://www.superheroapi.com/api.php/cfc55c379d941f22661c7167750d91b9/search/$superHeroName"
     CoroutineScope(Dispatchers.IO).launch {
-        /*val testResponse = httpClient.get(url)
-        response = testResponse.bodyAsText()*/
+        val response = httpClient.get(url).body<ApiResponse>()
+        onSuccessResponse(response.results)
     }
 }
 
---
+++ composeApp/src/commonMain/kotlin/network/model/Hero.kt
@@ -0,0 +1,6 @@
+package network.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Hero(val id: String, val name: String)
