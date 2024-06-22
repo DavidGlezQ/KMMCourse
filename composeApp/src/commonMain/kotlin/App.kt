3a15a6742f071d941107249223f70049bcf8fbc1
David Gonzalez <62410785+DavidGlezQ@users.noreply.github.com>
2024-06-21 23:06:20 -0600

    Simple API Call

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,7 +1,9 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,18 +27,33 @@ fun App() {
     MaterialTheme {
         val url = "https://www.superheroapi.com/api.php/cfc55c379d941f22661c7167750d91b9/search/a"
         var response by remember { mutableStateOf("Waiting...") }
+        var superHeroName by remember { mutableStateOf("") }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Row {
+                OutlinedTextField(value = superHeroName, onValueChange = { superHeroName = it })
+                Button(onClick = { getSuperHeroList(superHeroName) }) {
+                    Text("Load")
+                }
+            }
             Button(onClick = {
                 response = "Loading..."
                 CoroutineScope(Dispatchers.IO).launch {
                     val testResponse = httpClient.get(url)
-                    testResponse.bodyAsText()
+                    response = testResponse.bodyAsText()
                 }
             }) {
                 Text("Call SuperHero API!")
             }
-            Text(response)
+            //Text(response)
         }
+    }
+}
+
+fun getSuperHeroList(superHeroName: String) {
+    if (superHeroName.isBlank()) return
+    CoroutineScope(Dispatchers.IO).launch {
+        /*val testResponse = httpClient.get(url)
+        response = testResponse.bodyAsText()*/
     }
 }
 
